9ffc92f9ac0194a808bc57faf9ad75c2ddfb6fed
jvail <6503378+jvail@users.noreply.github.com>
2020-09-11 12:51:55 +0400

    Fix magics registration

--- pgljupyter/__init__.py
+++ pgljupyter/__init__.py
@@ -4,5 +4,7 @@ from ._version import __version__, version_info
 from .nbextension import _jupyter_nbextension_paths
 from .magics import PGLMagics
 
-if 'get_ipython' in globals() :
+try:
     get_ipython().register_magics(PGLMagics)
+except NameError:
+    pass
