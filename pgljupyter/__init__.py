3c6cbccc6c0d480a6401259dc55cb95c95a233ad
Frederic Boudon <frederic.boudon@cirad.fr>
2020-09-11 10:12:08 +0200

    change get_ipython existence test in __init__

--- pgljupyter/__init__.py
+++ pgljupyter/__init__.py
@@ -4,5 +4,5 @@ from ._version import __version__, version_info
 from .nbextension import _jupyter_nbextension_paths
 from .magics import PGLMagics
 
-if get_ipython is not None:
+if 'get_ipython' in globals() :
     get_ipython().register_magics(PGLMagics)
