8e6b7df2ca62c719ccb8dc3275444b809e941be3
jvail <6503378+jvail@users.noreply.github.com>
2020-10-08 11:39:12 +0400

    Add ifb script

--- ifb/ifb.sh
+++ ifb/ifb.sh
@@ -3,7 +3,7 @@ conda install -y -c fredboudon -c conda-forge openalea.lpy toml future rpy2 && \
 pip install git+https://github.com/fredboudon/deploy.git && \
 pip install git+https://github.com/fredboudon/mtg.git && \
 jupyter labextension install --no-build @jupyter-widgets/jupyterlab-manager && \
-jupyter labextension install --no-build pgljupyter && \
+pip install pgljupyter && \
 jupyter lab build && \
 git clone https://github.com/jvail/vmango.git && \
 python vmango/setup.py develop
